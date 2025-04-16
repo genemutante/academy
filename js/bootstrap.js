71a52d33278ee3d53fa44fb3db1e206f4b73e56e
genemutante <33362235+genemutante@users.noreply.github.com>
2025-04-16 11:15:04 -0300

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -9,12 +9,11 @@ import { narrar, exibirMensagemAluno } from './narrativa.js';
 import { initPlayer } from './initPlayer.js';
 import { onPlayerReady } from './onPlayerReady.js';
 import { verificarConclusaoAula } from './verificarConclusaoAula.js';
+import { supabase } from './supabaseClient.js';
 
-// 🔌 Supabase client global
-window.supabase = window.supabase.createClient(
-  'https://bkueljjvhijojzcyodvk.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrdWVsamp2aGlqb2p6Y3lvZHZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzUwNTQ4OSwiZXhwIjoyMDU5MDgxNDg5fQ.o-G5KmxoyfQjeNM5e7rbgxpryOHYC9k1OIo9fYzyeYE'
-);
+
+window.supabase = supabase;
+
 
 // 🌐 Params da URL
 const url = new URL(location.href);
